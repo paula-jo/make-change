4695f292c8dcf46b85e77a8562357f08afa6fe58
Paula Adans <54326845+paula-jo@users.noreply.github.com>
2019-09-23 15:00:51 -0600

    Add files via upload

--- money.py
+++ money.py
@@ -1,35 +1,30 @@
+from services import validation
+
 currencies = {
-    .01: 0,
-    .05: 0,
-    .10: 0,
-    .25: 0,
-    1.00: 0,
-    5.00: 0,
-    10.00: 0,
-    20.00: 0,
+    100.00: 0,
     50.00: 0,
-    100.00: 0
+    20.00: 0,
+    10.00: 0,
+    5.00: 0,
+    1.00: 0,
+    .25: 0,
+    .10: 0,
+    .05: 0,
+    .01: 0
 }
-
-ordered_denominations = sorted(currencies.keys())
-
-ordered_denominations.reverse()
 
 
 def make_change(entered_value):
+    validation_result = validation.money_validation(entered_value)
+    if not validation_result["is_valid"]:
+        print(validation_result["reason"])
+        return validation_result["reason"]
 
-    if entered_value < 0:
-        print("Sorry, I need a positive value!")
-        return "Sorry, I need a positive value!"
+    entered_value = float(entered_value)
 
-    for currency in ordered_denominations:
+    for currency in currencies.keys():
         x = entered_value % currency
         currencies[currency] = (entered_value - x) / currency
         entered_value = entered_value - currencies[currency] * currency
 
-    print("You'll need: ")
-
-    for quantity in currencies:
-        if currencies[quantity] > 0:
-            print(f"{currencies[quantity]} x ${quantity}.")
-
+    return currencies
