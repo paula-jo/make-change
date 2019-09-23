82c3ad1ccc83490591e33fc5a0cbcc9fcb89c90a
Paula Adans <54326845+paula-jo@users.noreply.github.com>
2019-09-23 15:00:19 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
 from services import money
+from services.money import currencies
 
 
 def main():
-    going = True;
-    while going:
-        try:
-            value = input("Please enter the amount you'd like to make change for:  ")
-            print("You entered: " + value)
-            money.make_change(float(value))
-            going = False
-        except ValueError:
-            print("No letters or spaces please!")
+    value = input("Please enter the amount you'd like to make change for:  ")
+    print("You entered: " + value)
+    money.make_change(value)
+
+    for quantity in currencies:
+        if currencies[quantity] > 0:
+            print(f"{currencies[ quantity ]} x ${quantity}.")
 
 
 main()
